7bf78b81d1d8aeb342a95754e5dadd82f8a0e2f0
kenzo <kenzou.kenzou104809@gmail.com>
2020-11-29 20:03:51 +0900

    fix relation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,4 @@
 class Product < ApplicationRecord
   validates :name, :price, presence: true
+  has_many :orders
 end
--- db/migrate/20201129104824_create_orders.rb
+++ db/migrate/20201129104824_create_orders.rb
@@ -2,6 +2,7 @@ class CreateOrders < ActiveRecord::Migration[6.0]
   def change
     create_table :orders do |t|
       t.integer :amount, null: false
+      t.references :product, null: false, foreign_key: true
 
       t.timestamps
     end
