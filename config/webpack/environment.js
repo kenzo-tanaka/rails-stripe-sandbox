12a4a5a3b5cd6658f8c86806f2e94df939905006
kenzo <kenzou.kenzou104809@gmail.com>
2020-11-30 19:48:29 +0900

    feat: setup jquery

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -20,4 +20,11 @@ environment.plugins.prepend('Environment',
   )
 )
 
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery'
+  })
+)
+
 module.exports = environment
