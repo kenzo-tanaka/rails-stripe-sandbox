b46b6a6c69ea16562abc5af82a3b0aa248955b3f
kenzo <kenzou.kenzou104809@gmail.com>
2020-11-30 19:47:01 +0900

    fix: environment.js

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,23 @@
 const { environment } = require('@rails/webpacker')
 
+// ref: https://qiita.com/hidehiro98/items/26e401687efb4901777c
+const webpack = require('webpack')
+const dotenv = require('dotenv')
+
+const dotenvFiles = [
+  `.env.${process.env.NODE_ENV}.local`,
+  '.env.local',
+  `.env.${process.env.NODE_ENV}`,
+  '.env'
+]
+dotenvFiles.forEach((dotenvFile) => {
+  dotenv.config({ path: dotenvFile, silent: true })
+})
+
+environment.plugins.prepend('Environment',
+  new webpack.EnvironmentPlugin(
+    JSON.parse(JSON.stringify(process.env))
+  )
+)
+
 module.exports = environment
